9362bdc1542dd2d3d79aa8ccf4ca6744c177ee20
t3yamoto <3yamoto.dev@gmail.com>
2021-07-06 23:01:23 +0900

    2-6

--- ch2/tasks_proj/tests/func/test_add.py
+++ ch2/tasks_proj/tests/func/test_add.py
@@ -1,10 +1,19 @@
-"""
-Placeholder test file.
-
-We'll add a bunch of tests here in later versions.
-"""
+import pytest
+import tasks
+from tasks import Task
 
 
-def test_add():
-    """Placeholder test."""
-    pass
+def test_add_returns_valid_id():
+    new_task = Task("do something")
+    task_id = tasks.add(new_task)
+    assert isinstance(task_id, int)
+
+
+@pytest.mark.smoke
+def test_added_task_has_id_set():
+    new_task = Task("sit in chair", owner="me", done=True)
+    task_id = tasks.add(new_task)
+
+    task_from_db = tasks.get(task_id)
+
+    assert task_from_db.id == task_id
