72f80b378ebada35696f6c6bc935461eef348884
t3yamoto <3yamoto.dev@gmail.com>
2021-07-06 22:48:09 +0900

    2-5

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -12,3 +12,16 @@ def test_start_tasks_db_raises():
         tasks.start_tasks_db("some/great/path", "mysql")
     exception_msg = excinfo.value.args[0]
     assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+
+@pytest.mark.smoke
+def test_list_tasks_raises():
+    with pytest.raises(TypeError):
+        tasks.list_tasks(owner=123)
+
+
+@pytest.mark.get
+@pytest.mark.smoke
+def test_get_raises():
+    with pytest.raises(TypeError):
+        tasks.get(task_id="123")
