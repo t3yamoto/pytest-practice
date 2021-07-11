8f02dc1c27ca0e8d9b9958f9856890970881a513
t3yamoto <3yamoto.dev@gmail.com>
2021-07-11 22:43:50 +0900

    2-13

---
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -0,0 +1,21 @@
+import pytest
+import tasks
+from tasks import Task
+
+
+def test_add_1():
+    task = Task("breathe", "BRIAN", True)
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+def equivalent(t1: Task, t2: Task):
+    return t1.summary == t2.summary and t1.owner == t2.owner and t1.done == t2.done
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    tasks.start_tasks_db(str(tmpdir), "tiny")
+    yield
+    tasks.stop_tasks_db()
