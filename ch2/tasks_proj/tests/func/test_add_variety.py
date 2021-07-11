44cefb0bce0b23837f1c67b5cb6692a8fa30ee1a
t3yamoto <3yamoto.dev@gmail.com>
2021-07-11 22:48:58 +0900

    2-14

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -10,6 +10,21 @@ def test_add_1():
     assert equivalent(t_from_db, task)
 
 
+@pytest.mark.parametrize(
+    "task",
+    [
+        Task("sleep", done=True),
+        Task("wake", "brian"),
+        Task("breathe", "brian", True),
+        Task("exercise", "BrIaN", False),
+    ],
+)
+def test_add_2(task):
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
 def equivalent(t1: Task, t2: Task):
     return t1.summary == t2.summary and t1.owner == t2.owner and t1.done == t2.done
 
